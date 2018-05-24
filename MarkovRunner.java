ab286c1da4729fb3c4175ebf9a3dbb45ccb220b6
Davide Nastri <d.nastri@gmail.com>
2018-05-24 13:54:46 +0200

    feat: Added MarkovWordOne class

--- MarkovRunner.java
+++ MarkovRunner.java
@@ -2,38 +2,58 @@
 /**
  * Write a description of class MarkovRunner here.
  * 
- * @author Duke Software
- * @version 1.0
+ * @author (your name) 
+ * @version (a version number or a date)
  */
 
 import edu.duke.*;
 
 public class MarkovRunner {
-    public void runMarkovZero() {
-		FileResource fr = new FileResource();
-		String st = fr.asString();
-		st = st.replace('\n', ' ');
-		MarkovZero markov = new MarkovZero();
-		markov.setTraining(st);
-		for(int k=0; k < 3; k++){
-			String text = markov.getRandomText(500);
-			printOut(text);
-		}
-	}
-	
-	private void printOut(String s){
-		String[] words = s.split("\\s+");
-		int psize = 0;
-		System.out.println("----------------------------------");
-		for(int k=0; k < words.length; k++){
-			System.out.print(words[k]+ " ");
-			psize += words[k].length() + 1;
-			if (psize > 60) {
-				System.out.println();
-				psize = 0;
-			}
-		}
-		System.out.println("\n----------------------------------");
-	}
-	
+    public void runModel(IMarkovModel markov, String text, int size){ 
+        markov.setTraining(text); 
+        System.out.println("running with " + markov); 
+        for(int k=0; k < 3; k++){ 
+            String st = markov.getRandomText(size); 
+            printOut(st); 
+        } 
+    } 
+
+    public void runModel(IMarkovModel markov, String text, int size, int seed){ 
+        markov.setTraining(text); 
+        markov.setRandom(seed);
+        System.out.println("running with " + markov); 
+        for(int k=0; k < 3; k++){ 
+            String st = markov.getRandomText(size); 
+            printOut(st); 
+        } 
+    } 
+
+    public void runMarkov() { 
+        FileResource fr = new FileResource(); 
+        String st = fr.asString(); 
+        st = st.replace('\n', ' '); 
+        MarkovWordOne markovWord = new MarkovWordOne(); 
+        runModel(markovWord, st, 200); 
+    } 
+
+    private void printOut(String s){
+        String[] words = s.split("\\s+");
+        int psize = 0;
+        System.out.println("----------------------------------");
+        for(int k=0; k < words.length; k++){
+            System.out.print(words[k]+ " ");
+            psize += words[k].length() + 1;
+            if (psize > 60) {
+                System.out.println(); 
+                psize = 0;
+            } 
+        } 
+        System.out.println("\n----------------------------------");
+    }
+
+    public static void main(String[] args) {
+        MarkovRunner mr = MarkovRunner();
+        mr.runMarkov();
+    }
+
 }
