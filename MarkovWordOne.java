5d9bd85c5c67e295b21de59f1b3388c866586bb5
Davide Nastri <d.nastri@gmail.com>
2018-05-25 15:23:22 +0200

    feat: Added testIndexOf method in MarkovWordOne class

--- MarkovWordOne.java
+++ MarkovWordOne.java
@@ -23,7 +23,24 @@ public class MarkovWordOne implements IMarkovModel {
     public void setTraining(String text){
 		myText = text.split("\\s+");
 	}
-	
+
+	public void testIndexOf() {
+    	String testString = "this is just a test yes this is a simple test";
+		String[] words = testString.split("\\s+");
+		indexOf(words, "yes", 0);
+	}
+
+	private int indexOf(String[] words, String target, int start) {
+		for (int i = start; i < words.length; i++) {
+			if (words[i].equals(target)) {
+				System.out.println("Found \"" + words[i] + "\" at index " + i);
+				return i;
+			}
+
+		}
+    	return -1;
+	}
+
 	public String getRandomText(int numWords){
 		StringBuilder sb = new StringBuilder();
 		int index = myRandom.nextInt(myText.length-1);  // random word to start with
@@ -49,5 +66,10 @@ public class MarkovWordOne implements IMarkovModel {
 	    ArrayList<String> follows = new ArrayList<String>();
 	    return follows;
     }
+
+	public static void main(String[] args) {
+		MarkovWordOne mwo = new MarkovWordOne();
+		mwo.testIndexOf();
+	}
 
 }
